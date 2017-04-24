bd5fbea97c56ccf2a583e71be0ce1bfc7f983d5b
Tim Mioduszewski <tim_mioduszewski@yahoo.com>
2017-04-24 11:55:48 -0700

    test current user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
   has_attached_file :image, styles: { medium: "500x500>", small: "250x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
